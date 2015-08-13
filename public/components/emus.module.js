aa184a6beb3cdf8c699398bd61cd1043ed760764
pokaxperia <pokazzam@gmail.com>
2015-08-13 14:03:12 -0500

    module emus.modalities added, on  function added with var nombre

--- public/components/emus.module.js
+++ public/components/emus.module.js
@@ -5,15 +5,23 @@
 	angular.module('emus',[
 		'emus.routes',
 		'emus.landing',
+		'emus.modalities',
 		'emus.calculator',
 		'duScroll'
 		]
 	)
 	.value('duScrollDuration', 750)
-	.run(["$rootScope", "$state", "$stateParams",function ($rootScope, $state, $stateParams) {
+	.run(["$rootScope", "$state", "$stateParams", function ($rootScope, $state, $stateParams) {
+
 		$rootScope.$state = $state;
 		$rootScope.$stateParams = $stateParams;
+		
+		$rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams){
+			$rootScope.nombre = toState.name;
+		});
+
 		return $rootScope;
+
 	}]);
 
 }());
