b1ad5386b4bf7eae0ebf47e0076cf0f070983ec1
pokaxperia <pokazzam@gmail.com>
2015-08-16 01:38:37 -0500

    minimal changes

--- public/components/calculator/calculator.factory.js
+++ public/components/calculator/calculator.factory.js
@@ -1,10 +1,10 @@
 /**
-* Service Module
+* Factory Module
 *
 * Description
 */
 angular.module('emus.calculator.factory', []).
-factory('SendQuote', ['$http', '$q',function($http, $q){
+factory('SendQuote', ['$http', '$q', function($http, $q){
 	var deferred = $q.defer();
 	return {
 		sendQuote: function(quote_values){
@@ -15,7 +15,6 @@ factory('SendQuote', ['$http', '$q',function($http, $q){
 				deferred.reject(error);
 			});
 		}
-		
 	};
 	return deferred.promise;
 }]);
