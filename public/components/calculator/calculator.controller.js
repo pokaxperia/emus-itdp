09cfcf1eb74100f685679ea57a5d3dc571b7776d
pokaxperia <pokazzam@gmail.com>
2015-08-15 19:03:48 -0500

    watch function for kmevaluables field and automatic calcule for pozosproyecto field ready, function to switch between automatic calcule and manual input ready.

--- public/components/calculator/calculator.controller.js
+++ public/components/calculator/calculator.controller.js
@@ -7,24 +7,43 @@
 	var CalculatorController = function($scope,$location, $state, $log){
 
 		var tipo;
+		$scope.bandera = false;
 		$scope.calculator = {};
-		var getQuotesValues = sessionStorage.getItem('valor');
+		var getQuote = sessionStorage.getItem('setQuote');
+		var flag = sessionStorage.getItem('flag');
+
+		if(getQuote  !== null){
+			$scope.calculator = JSON.parse(getQuote);
+			$log.info("sessionStorage not empty");
+			if (flag == 'true') {
+				$scope.flag = true;
+			}
+			else{
+				$scope.flag = false;
+			}
+		}
+
 		
-		if(Object.keys($scope.calculator).length === 0){
+/*	if(Object.keys($scope.calculator).length === 0){
 			$log.warn("Calculator object value: " +Object.keys($scope.calculator).length);
-			if(getQuotesValues === null){
+			if(getQuote === null){
 				$log.warn("sessionStorage empty");
 				sessionStorage.clear();
 				$log.info("Cleaning sessionStorage");
 			}
 			else{
+				
 				$log.info("sessionStorage not empty");
-				var quotes = JSON.parse(getQuotesValues);
+				var quotes = JSON.parse(getQuote);
 				$scope.calculator = quotes;
+				if (bandera == 'true') {
+					$scope.bandera = true;
+				}
 			}
-		}
+		}*/
 
 		$scope.getType = function(tipo){
+			$log.info("sessionStorage not empty");
 			tipo = tipo.infraestructura;
 			if(tipo){
 				switch($scope.calculator.infraestructura){
@@ -38,18 +57,48 @@
 						$scope.calculator.tipo_calle = "Primaria";
 						break;
 				}
-				var valores = JSON.stringify($scope.calculator);
-				sessionStorage.setItem('valor',valores);
-				$state.go('modalidades.calculadora.formulario');
 			}
+
+			var valores = JSON.stringify($scope.calculator);
+			sessionStorage.setItem('setQuote',valores);
+			$state.go('modalidades.calculadora.formulario');
+
+		};
+		
+		if (flag == 'true') {
+			$scope.$watch('calculator.KmEvaluables', function(newValue, oldValue){
+					sessionStorage.setItem('flag','true');
+					$scope.flag = true; 
+					$scope.calculator.PozosProyecto = (newValue * 1000) / 200;
+					$scope.valores = $scope.calculator.PozosProyecto;
+			});
+		}
+
+		$scope.getPozosAuto = function(valor){
+			sessionStorage.setItem('flag','true');
+			$scope.flag = true;
+
+			$scope.$watch('calculator.KmEvaluables', function(newValue, oldValue){
+				sessionStorage.setItem('flag','true');
+				$scope.flag = true;
+				$scope.calculator.PozosProyecto = (newValue * 1000) / 200;
+				$scope.valores = $scope.calculator.PozosProyecto;
+			});
 		};
 
+		$scope.getPozosCustom = function(){
+			sessionStorage.setItem('flag','false');
+			$scope.flag = false;
+			$scope.$watch( 'calculator.PozosProyecto', function(newValue, oldValue){
+				$scope.nuevosvalores = newValue;
+			});
+		};
+		//Form
 		$scope.saveQuote = function(calculatorForm, calculator){
-			console.log(calculatorForm);
 			if (calculatorForm.$valid === true) {
 				$scope.calculatorForm.submitted = true;
 				var valores = JSON.stringify(calculator);
-				sessionStorage.setItem('valor',valores);
+				sessionStorage.setItem('setQuote',valores);
 				$state.go('modalidades.calculadora.formulario');
 			}
 		};
