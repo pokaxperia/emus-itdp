c6b6449e7e9c2ad3ea7d9377a3d9b67bd63ffee2
pokaxperia <pokazzam@gmail.com>
2015-08-31 15:07:31 -0500

    new directories

--- src/client/components/calculator/calculator.factory.js
+++ src/client/components/calculator/calculator.factory.js
@@ -6,17 +6,17 @@
 	*/
 	angular.module('emus.calculator.factory', []).
 	factory('SendQuote', ['$http', '$q', function($http, $q){
-		var deferred = $q.defer();
 		return {
 			sendQuote: function(quote_values){
+				var deferred = $q.defer();
 				return $http.post('http://itdp.mx/emus/#/', quote_values).
 				then(function(result){
 					deferred.resolve(result);
 				}, function(error){
 					deferred.reject(error);
 				});
+				return deferred.promise;
 			}
 		};
-		return deferred.promise;
 	}]);
 })();
