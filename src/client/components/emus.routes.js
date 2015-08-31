a44f2adaa70b188bd56e74a52b63a9d011396530
pokaxperia <pokazzam@gmail.com>
2015-08-31 15:11:43 -0500

    testing breadcrumbs

--- src/client/components/emus.routes.js
+++ src/client/components/emus.routes.js
@@ -2,35 +2,57 @@
 	'use strict';
 	
 	angular.module('emus.routes', ['ui.router'])
-	.config(['$stateProvider', '$urlRouterProvider',function($stateProvider, $urlRouterProvider) {
+	.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
 		$urlRouterProvider.otherwise('/');
 		$stateProvider
-			.state('index', {
+			.state('inicio', {
 				url: '/',
 				templateUrl: './components/landing/landing.html',
 				controller: 'LandingController'
 			})
 			.state('modalidades', {
+				abstract: false,
 				url: '/modalidades',
 				templateUrl: './components/modalities/modalities.html',
-				controller: 'ModalitiesController'
+				controller: 'ModalitiesController',
+				ncyBreadcrumb: {
+					label: 'MODALIDADES',
+					parent: 'inicio'
+				}
 			})
-			.state('modalidades.calles', {
-				url: '/calles',
-				templateUrl: './components/modalities/streets/streets.html'
+			.state('modalidades.tipo', {
+				url: '/:tipo',
+				templateUrl: function($stateParams){
+					return './components/modalities/type/'+$stateParams.tipo+'.html';
+				},
+				ncyBreadcrumb: {
+					label: "{{ $stateParams.tipo }}",
+					parent: 'modalidades'
+				}
 			})
 			.state('modalidades.calculadora', {
 				url: '/calculadora',
 				templateUrl: './components/calculator/calculator.html',
+				onEnter: function($rootScope) {
+				  return $rootScope.pruebas;
+				},
 				controller: 'CalculatorController'
 			})
 			.state('modalidades.calculadora.tipo-de-proyecto', {
 				url: '/tipo-de-proyecto',
-				templateUrl: './components/calculator/type-of-project.html'
+				templateUrl: './components/calculator/type/type-of-project.html',
+				ncyBreadcrumb: {
+					label: "PROYECTO",
+					parent: 'modalidades.calculadora'
+				}
 			})
 			.state('modalidades.calculadora.formulario', {
 				url: '/formulario',
-				templateUrl: './components/calculator/form.html'
+				templateUrl: './components/calculator/form.html',
+				ncyBreadcrumb: {
+					label: "FORMULARIO",
+					parent: 'modalidades.calculadora.tipo-de-proyecto'
+				}
 			})
 			.state('modalidades.calculadora.resumen', {
 				url: '/resumen',
