fec4fdbb12fd077cfaff75bed0fa6a6abc8bd048
Mario FLores <pokazzam@gmail.com>
2015-10-05 15:54:51 -0500

    removed commented code

--- src/client/components/calculator/form/form.factory.js
+++ src/client/components/calculator/form/form.factory.js
@@ -7,29 +7,17 @@
 	angular.module('emus.form.factory', []).
 	factory('SendQuote', ['$http', '$q', function($http, $q){
 		return {
-			/*sendQuote: function(quote_values){
-				var data = JSON.stringify(quote_values);
-				console.log(msg)
-				var deferred = $q.defer();
-				$http.post('api/index.php/estimate', data).
-				then(function(result){
-					deferred.resolve(result);
-				}, function(error){
-					deferred.reject(error);
-				});
-				return deferred.promise;
-			}*/
 			sendQuote: function(quote_values){
 				var data = JSON.stringify(quote_values);
 				var deferred = $q.defer();
 
 				var response = $http({
-				  method: 'POST',
-				  url: 'api/index.php/estimate',
-				  data: data,
-				  headers: {
-				    'Content-Type': 'application/json'
-				  }
+					method: 'POST',
+					url: 'api/index.php/estimate',
+					data: data,
+					headers: {
+						'Content-Type': 'application/json'
+					}
 				});
 
 				response.then(function(result){
