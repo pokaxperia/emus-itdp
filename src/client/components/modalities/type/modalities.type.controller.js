94f526405f0493cf341698fb6d3a6b1ed36b0718
Mario FLores <pokazzam@gmail.com>
2015-12-09 22:47:46 -0600

    ngActivityIndicator added to  response request

--- src/client/components/modalities/type/modalities.type.controller.js
+++ src/client/components/modalities/type/modalities.type.controller.js
@@ -4,19 +4,25 @@
 	*/
 	'use strict';
 
-	var ModalityTypeController = function($log, $scope, $rootScope, $stateParams, $state, ModalityFactory){
+	var ModalityTypeController = function($log, $scope, $rootScope, $stateParams, $state, $activityIndicator, $timeout,ModalityFactory){
 		var type_modality = $stateParams.tipo;
-
+		$activityIndicator.startAnimating();
 		ModalityFactory.getModality(type_modality).
 		then(function(result){
-			$scope.set_m = result;
+			$activityIndicator.stopAnimating();
+			$timeout(function () {
+				if (result.proyectos === false) {
+					$scope.mensaje = false;
+				}
+				$scope.set_m = result;
+			}, 1000);
 		}, function(error){
 			$log.error(error);
 		});
 
 	};
 	
-	ModalityTypeController.$inject = ['$log', '$scope', '$rootScope', '$stateParams', '$state','ModalityFactory'];
+	ModalityTypeController.$inject = ['$log', '$scope', '$rootScope', '$stateParams', '$state', '$activityIndicator', '$timeout', 'ModalityFactory'];
 	
 	angular.module('emus.modalities.type', []).
 	controller('ModalityTypeController', ModalityTypeController);
